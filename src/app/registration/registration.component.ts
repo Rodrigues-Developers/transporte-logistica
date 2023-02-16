14f5c0e6e26b57d2df0678b38b01d9ba0f743811
Bruno RC <brunorcx@hotmail.com>
2023-02-16 08:10:27 -0400

    feat: added validation for input registration page

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
-  styleUrls: ['./registration.component.less']
+  styleUrls: ['./registration.component.less'],
 })
-export class RegistrationComponent {
+export class RegistrationComponent implements OnInit {
+  registrationForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.registrationForm = this.fb.group({
+      nfe: ['', Validators.required],
+      emissionDate: ['', Validators.required],
+      transport: ['', Validators.required],
+      departureDate: ['', Validators.required],
+      arrivalForecast: ['', Validators.required],
+      merchandise: ['', Validators.required],
+      amount: ['', [Validators.required, Validators.pattern('[0-9]+')]],
+      nfValue: [
+        '',
+        [Validators.required, Validators.pattern('[0-9]+(.[0-9]{1,2})?')],
+      ],
+      pinRelease: ['', Validators.required],
+      obs: [''],
+    });
+  }
+  onSubmit() {
+    //Send Changes
+  }
+
+  ngOnInit() {}
 }
