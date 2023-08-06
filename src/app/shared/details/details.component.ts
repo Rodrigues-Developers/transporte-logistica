1af53c02d205a1a0fb1b4239bd3239d77ab86c64
Machado21 <grmachado12@hotmail.com>
2023-08-06 00:12:44 -0400

    fix: details page error has been fixed. I just need to fix the front end.

--- src/app/shared/details/details.component.ts
+++ src/app/shared/details/details.component.ts
@@ -1,13 +1,10 @@
-import { DataShareService } from './../../core/services/data-share.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ProductService } from 'src/app/core/services/product.service';
-import { LogisticService } from 'src/app/core/services/logistic.service';
 import { Product } from 'src/app/core/interfaces/product.interface';
 import { ActivatedRoute } from '@angular/router';
-import { ObjectId } from 'mongodb';
 import { Logistic } from 'src/app/core/interfaces/logistic.interface';
-import { Subject } from 'rxjs/internal/Subject';
-import { Observable } from 'rxjs';
+import { DataShareService } from './../../core/services/data-share.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-details',
@@ -15,9 +12,7 @@ import { Observable } from 'rxjs';
   styleUrls: ['./details.component.less'],
 })
 
-// TODO turn this page generic to use in any component
-
-export class DetailsComponent implements OnInit {
+export class DetailsComponent implements OnInit, OnDestroy {
   product = {} as Product;
   products: Product[] = [];
   productsNfe: Product[] = [];
@@ -25,33 +20,42 @@ export class DetailsComponent implements OnInit {
   logistic = {} as Logistic;
   logistics: Logistic[] = [];
 
-  //Lib to observe the changes in the logistic.
-  private logisticObservable: Observable<Logistic>;
+  private logisticObservableSubscription: Subscription = new Subscription();
 
   constructor(
     private route: ActivatedRoute,
-    private productService: ProductService, // private logisticService: LogisticService
-    private dataShareService: DataShareService // Received the data from another page
-  ) {
-    this.logisticObservable = this.dataShareService.getDataObservable(); // Get the data from another page
-  }
+    private productService: ProductService,
+    private dataShareService: DataShareService
+  ) {}
 
   ngOnInit(): void {
-    this.getProduct(this.logistic);
+    this.logisticObservableSubscription = this.dataShareService
+      .getDataObservable()
+      .subscribe((newLogistic: Logistic) => {
+        this.logistic = newLogistic;
+        this.getProduct(this.logistic);
+      });
+  }
+
+  ngOnDestroy(): void {
+    // Lembre-se de cancelar a inscrição ao destruir o componente
+    if (this.logisticObservableSubscription) {
+      this.logisticObservableSubscription.unsubscribe();
+    }
   }
 
   async getProduct(nfe: any) {
-    try{
+    try {
+      this.productsNfe = []; // Limpar o array antes de adicionar novos produtos
       for (let i = 0; i < nfe.merchandise.length; i++) {
         await this.productService
           .getProductById(nfe.merchandise[i])
           .subscribe((product: Product) => {
             this.productsNfe.push(product);
-            console.log("in");
           });
-    }
-    } catch {
-      console.error("Details error")
+      }
+    } catch (error) {
+      console.error("Details error", error);
     }
   }
 }
