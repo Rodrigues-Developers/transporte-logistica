62a323fcb787ebd070b5fa0f046446bce9704a2b
brunorcx <brunorcx@hotmail.com>
2023-11-19 16:51:57 -0400

    feat: responsive header for all pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,18 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { TrackingComponent } from './features/tracking/tracking.component';
-import { RegistrationComponent } from './features/registration/registration.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
-import { ProductsTrackingComponent } from './features/products-tracking/products-tracking.component';  
-import { ProductService } from 'src/app/core/services/product.service';
-import { LogisticService } from 'src/app/core/services/logistic.service';
-import { DetailsComponent } from './shared/details/details.component';
-import { DeliveredComponent } from './features/delivered/delivered.component';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { TrackingComponent } from "./features/tracking/tracking.component";
+import { RegistrationComponent } from "./features/registration/registration.component";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule, HttpClient } from "@angular/common/http";
+import { ProductsTrackingComponent } from "./features/products-tracking/products-tracking.component";
+import { ProductService } from "src/app/core/services/product.service";
+import { LogisticService } from "src/app/core/services/logistic.service";
+import { DetailsComponent } from "./shared/details/details.component";
+import { DeliveredComponent } from "./features/delivered/delivered.component";
+import { AngularMaterialModule } from "./core/modules/angular-material-module/angular-material-module";
 
 @NgModule({
   declarations: [
@@ -30,10 +31,10 @@ import { DeliveredComponent } from './features/delivered/delivered.component';
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
+    AngularMaterialModule
   ],
 
   providers: [HttpClientModule, ProductService, LogisticService],
   bootstrap: [AppComponent]
 })
-
-export class AppModule { }
+export class AppModule {}
