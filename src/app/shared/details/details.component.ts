292e9b74dd65ea5fd2091c23a6723467e4413d33
brunorcx <brunorcx@hotmail.com>
2024-01-25 20:50:38 -0400

    chore: updating angular and other libs

--- src/app/shared/details/details.component.ts
+++ src/app/shared/details/details.component.ts
@@ -1,17 +1,15 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { ProductService } from 'src/app/core/services/product.service';
-import { Product } from 'src/app/core/interfaces/product.interface';
-import { ActivatedRoute } from '@angular/router';
-import { Logistic } from 'src/app/core/interfaces/logistic.interface';
-import { DataShareService } from './../../core/services/data-share.service';
-import { Subscription } from 'rxjs';
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { ProductService } from "src/app/core/services/product.service";
+import { Product } from "src/app/core/interfaces/product.interface";
+import { Logistic } from "src/app/core/interfaces/logistic.interface";
+import { DataShareService } from "./../../core/services/data-share.service";
+import { Subscription } from "rxjs";
 
 @Component({
-  selector: 'app-details',
-  templateUrl: './details.component.html',
-  styleUrls: ['./details.component.less'],
+  selector: "app-details",
+  templateUrl: "./details.component.html",
+  styleUrls: ["./details.component.less"]
 })
-
 export class DetailsComponent implements OnInit, OnDestroy {
   product = {} as Product;
   products: Product[] = [];
@@ -22,11 +20,7 @@ export class DetailsComponent implements OnInit, OnDestroy {
 
   private logisticObservableSubscription: Subscription = new Subscription();
 
-  constructor(
-    private route: ActivatedRoute,
-    private productService: ProductService,
-    private dataShareService: DataShareService
-  ) {}
+  constructor(private productService: ProductService, private dataShareService: DataShareService) {}
 
   ngOnInit(): void {
     this.logisticObservableSubscription = this.dataShareService
@@ -47,11 +41,9 @@ export class DetailsComponent implements OnInit, OnDestroy {
     try {
       this.productsNfe = []; // Limpar o array antes de adicionar novos produtos
       for (let i = 0; i < nfe.merchandise.length; i++) {
-        await this.productService
-          .getProductById(nfe.merchandise[i])
-          .subscribe((product: Product) => {
-            this.productsNfe.push(product);
-          });
+        await this.productService.getProductById(nfe.merchandise[i]).subscribe((product: Product) => {
+          this.productsNfe.push(product);
+        });
       }
     } catch (error) {
       console.error("Details error", error);
