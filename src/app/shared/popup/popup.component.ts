20ea3e913ed57d2953a70632d83297d33d1c37c0
brunorcx <brunorcx@hotmail.com>
2024-02-18 12:14:04 -0400

    feat: added popup after the user clicks on receive button in the tracking page

---
+++ src/app/shared/popup/popup.component.ts
@@ -0,0 +1,66 @@
+import { Component, Input, Output, EventEmitter, ElementRef, ViewChild, AfterViewInit } from "@angular/core";
+import { AnimationBuilder, animate, style } from "@angular/animations";
+
+@Component({
+  selector: "app-popup",
+  templateUrl: "./popup.component.html",
+  styleUrls: ["./popup.component.less"]
+})
+export class PopupComponent {
+  private _isVisible: boolean = false;
+  player: any;
+
+  @Input()
+  set isVisible(value: boolean) {
+    this._isVisible = value;
+
+    if (this.popUpElement) {
+      this.setAnimation(this.popUpElement.nativeElement as HTMLElement, this._isVisible);
+    }
+  }
+
+  get isVisible(): boolean {
+    return this._isVisible;
+  }
+
+  @Output() isVisibleEmitter = new EventEmitter<boolean>();
+  @Output() confirmEmitter = new EventEmitter<boolean>();
+
+  @ViewChild("popUpElement", { static: false }) popUpElement!: ElementRef;
+
+  constructor(private animationBuilder: AnimationBuilder) {}
+
+  close() {
+    this._isVisible = false;
+    this.isVisibleEmitter.emit(this._isVisible);
+  }
+
+  confirm() {
+    this.confirmEmitter.emit(true);
+  }
+
+  back() {
+    this.confirmEmitter.emit(false);
+  }
+
+  setAnimation(element: HTMLElement, isVisble: boolean): void {
+    // Checks if the animation is running
+    if (this.player?.getPosition() < 1) {
+      this.player.reset();
+    }
+
+    const opacityValue = isVisble ? 1 : 0;
+
+    const metadata = [animate("0.4s ease-in-out", style({ opacity: opacityValue }))];
+
+    const factory = this.animationBuilder.build(metadata);
+    this.player = factory.create(element);
+    this.player.play();
+
+    this.player.onDone(() => {
+      if (!isVisble) {
+        this.close();
+      }
+    });
+  }
+}
