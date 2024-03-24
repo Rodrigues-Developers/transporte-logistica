148a918cf75a83719ecda3d20977e8e6d8c3cec0
brunorcx <brunorcx@hotmail.com>
2024-03-24 15:15:52 -0400

    fix: saving 1 or only 2 dates in the details form

--- src/app/core/utils/dates.ts
+++ src/app/core/utils/dates.ts
@@ -2,7 +2,7 @@ export function stringToDate(date: string) {
   const timestamp = Date.parse(date);
 
   if (isNaN(timestamp)) {
-    throw new Error("Invalid date string. Please provide a valid date string.");
+    return;
   }
 
   return new Date(timestamp);
