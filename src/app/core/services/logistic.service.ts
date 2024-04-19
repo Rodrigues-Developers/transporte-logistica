c212eaf737a6981411fd0b96d97531cd412a2785
Gabriel <grmachado12@hotmail.com>
2024-04-18 22:54:48 -0400

    feat: Grouping together and appearing ligistics without defined dates.

--- src/app/core/services/logistic.service.ts
+++ src/app/core/services/logistic.service.ts
@@ -14,8 +14,8 @@ var httpOptions = {
 export class LogisticService {
   constructor(private http: HttpClient) {}
 
-  url = "http://localhost:3000/";
-  urlOnline = "https://nodejs-api-logistica.onrender.com/";
+  urlLocal = "http://localhost:3000/";
+  url = "https://nodejs-api-logistica.onrender.com/";
   logistic = {} as Logistic;
 
   /**
